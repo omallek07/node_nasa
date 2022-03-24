3aca8693626a0777f98da5b0df89bb19b0eae75c
Kevin O'Malley <omallek@gmail.com>
2022-03-24 06:55:14 -0500

    connect addNewLaunch to client side

--- client/src/pages/Launch.js
+++ client/src/pages/Launch.js
@@ -3,8 +3,6 @@ import { Appear, Button, Loading, Paragraph } from "arwes";
 import Clickable from "../components/Clickable";
 
 const Launch = (props) => {
-  console.log("props", props);
-
   const selectorBody = useMemo(() => {
     return props.planets?.map((planet) => (
       <option value={planet.kepler_name} key={planet.kepler_name}>
