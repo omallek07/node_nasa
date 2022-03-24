49890887e2d4d66a9192d2bb29f2bd8ed822b40a
Kevin O'Malley <omallek@gmail.com>
2022-03-24 07:12:56 -0500

    implement abortLaunch functionality

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -27,6 +27,16 @@ async function httpSubmitLaunch(launch) {
   }
 }
 
-async function httpAbortLaunch(id) {}
+async function httpAbortLaunch(id) {
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: "delete",
+    });
+  } catch (err) {
+    return {
+      ok: false,
+    };
+  }
+}
 
 export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
