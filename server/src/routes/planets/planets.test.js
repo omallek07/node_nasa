4cd3d1623ad90ce9173040502c87ecb4b83381f6
Kevin O'Malley <omallek@gmail.com>
2022-03-29 07:13:21 -0500

    update jest tests to handle mongoose db

--- server/src/routes/planets/planets.test.js
+++ server/src/routes/planets/planets.test.js
@@ -1,36 +1,41 @@
 const request = require("supertest");
 const app = require("../../app");
-const { loadPlanetsData } = require("../../models/planets.model.js");
 
-describe("Test GET /planets", () => {
-  test("It should respond with 200 success", async () => {
-    await request(app)
-      .get("/planets")
-      .expect("Content-Type", /json/)
-      .expect(200);
-  });
-});
+const { mongoConnect, mongoDisconnect } = require("../../services/mongo");
 
-describe("Planets should have viable living conditions", () => {
-  /*
-  // planet["koi_disposition"] === "CONFIRMED" &&
-  //   planet["koi_insol"] > 0.36 &&
-  //   planet["koi_insol"] < 1.11 &&
-  //   planet["koi_prad"] < 1.6;
-  */
-
+describe("Planets API", () => {
   beforeAll(async () => {
-    // Load data from CSV file
-    await loadPlanetsData();
+    await mongoConnect();
   });
 
-  test("All  planets should have CONFIRMED koi_disposition", async () => {
-    const { body: planets } = await request(app).get("/planets");
+  afterAll(async () => {
+    await mongoDisconnect();
+  });
 
-    expect(planets).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({ koi_disposition: "CONFIRMED" }),
-      ])
-    );
+  describe("Test GET /planets", () => {
+    test("It should respond with 200 success", async () => {
+      await request(app)
+        .get("/planets")
+        .expect("Content-Type", /json/)
+        .expect(200);
+    });
+  });
+
+  describe("Planets should have viable living conditions", () => {
+    /*
+    // planet["koi_disposition"] === "CONFIRMED" &&
+    //   planet["koi_insol"] > 0.36 &&
+    //   planet["koi_insol"] < 1.11 &&
+    //   planet["koi_prad"] < 1.6;
+    */
+    test("All  planets should have CONFIRMED koi_disposition", async () => {
+      const { body: planets } = await request(app).get("/planets");
+
+      expect(planets).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ keplerName: expect.any(String) }),
+        ])
+      );
+    });
   });
 });
