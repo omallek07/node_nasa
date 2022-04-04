81ac65e6b7077884fa985bf40e92a28a564ca33b
Kevin O'Malley <omallek@gmail.com>
2022-04-04 07:04:08 -0500

    ready for deploy v1

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const MONGO_URL =
-  "mongodb+srv://nasa-api:barxah-4juqdi-hesPuz@nodenasacluster.suxec.mongodb.net/nasa?retryWrites=true&w=majority";
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready!");
